6565cfd97ea794b68b317d97429d60e032f1f787
takewiki <hulilei@takewiki.com.cn>
2018-10-03 09:24:42 +0800

    updatethe dir files.

--- R/02-dirOperating.R
+++ R/02-dirOperating.R
@@ -8,3 +8,32 @@ dir.getRHome <- function() {
   res <-R.home();
   return(res);
 }
+
+#' 获取指定目录下的所有文件名
+#'
+#' @param dir_name 目录
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples dir.getAllFileBaseName("./data-raw");
+dir.getAllFileBaseName <- function(dir_name) {
+ res <- dir(dir_name);
+ return(res);
+ # dir('./data-raw/UserManuals',full.names = TRUE);
+  
+}
+
+#' 获取指定目录下所有文件的全称
+#'
+#' @param dir_name 文件目录
+#'
+#' @return 返回值，此目录下所有文件的全称
+#' @export
+#'
+#' @examples dir.getAllFileFullName('./data-raw/')
+dir.getAllFileFullName <- function(dir_name) {
+  res <- dir(dir_name,full.names = TRUE);
+  return(res);
+  
+}
